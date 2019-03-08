a6ed6e5862599d18913d1ee67fb95dee6bd534dd
Gian Luca Rivera <gian.luca.99@hotmail.com>
2019-03-08 16:06:51 -0600

    Final

--- HT6/src/ht6/HT6.java
+++ HT6/src/ht6/HT6.java
@@ -26,7 +26,9 @@ public class HT6 {
      */
     public static void main(String[] args) {
         int mapType;
-        int optionMenu;
+        int optionMenu = 0;
+        
+         
         Scanner read = new Scanner(System.in);
         //Mazo de cartas
          Map<String, String> cards;
@@ -34,8 +36,9 @@ public class HT6 {
         
          MapFactory factory = new MapFactory();
          
-         ArrayList<String> cardName =new ArrayList();
+         ArrayList<String> cardName = new ArrayList();
          ArrayList<String> characters = new ArrayList();
+         ArrayList<String> cardNamePlayer = new ArrayList();
         
         //Bloque de cofigo que se encarga de la lectura del archivo de texto que contiene las cartas
         ArrayList<String> allCards = new ArrayList();
@@ -51,6 +54,7 @@ public class HT6 {
                 + "\n1. HashMap"
                 + "\n2. TreeMap"
                 + "\n3. LinkedHashMap");
+        
         mapType = read.nextInt();
         
         //Programacion defensiva, si ingresa un numero mayor a 3, el programa se cierra
@@ -72,69 +76,132 @@ public class HT6 {
                         //System.out.println(totalCards);
 	    }
         
-        System.out.println("Seleccione la operación que desea realizar: "
-                + "\n1. Agregar una carta a tu coleccion"
-                + "\n2. Mostrar el tipo de una carta"
-                + "\n3. Mostrar informacion de las cartas de tu mazo"
-                + "\n4. Mostrar informacion de las cartas de tu mazo ordenadas por tipo"
-                + "\n5. Mostrar informacion de todas las cartas existentes"
-                + "\n6. Mostrar informacion de todas las cartas existentes ordenadas por tipo");
-        optionMenu = read.nextInt();
         
-        switch(optionMenu){
-            case 1:
-                String name;
-                System.out.println("Ingrese el nombre de la carta que desea agregar a su coleccion: ");
-                name = read.next();
-                if (totalCards.containsKey(name) == true){
-                    characters.add(name);
-                    String newCard = totalCards.get(name);
-                    totalCards.remove(name);
-                    cards.put(name, newCard);
-                    System.out.println("La carta '" + name + "' se ha agregado a tu mazo.");
-                }else{
-                    System.out.println("La carta que ingresaste no esta dentro del mazo general.");
-                }
-                break;
+        
+        while(optionMenu != 7){
+            System.out.println("Seleccione la operación que desea realizar: "
+                        + "\n1. Agregar una carta a tu coleccion"
+                        + "\n2. Mostrar el tipo de una carta"
+                        + "\n3. Mostrar informacion de las cartas de tu mazo"
+                        + "\n4. Mostrar informacion de las cartas de tu mazo ordenadas por tipo"
+                        + "\n5. Mostrar informacion de todas las cartas existentes"
+                        + "\n6. Mostrar informacion de todas las cartas existentes ordenadas por tipo");
+            optionMenu = read.nextInt();
+                    
+            switch(optionMenu){
                 
-            case 2:
-                String name2;
-                System.out.println("Ingrese el nombre de la carta para saber de que tipo es: ");
-                name2 = read.next();
-                System.out.println("La carta '" + name2 + "' es de tipo:" + totalCards.get(name2));
-                break;
-                
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                //Ciclo for que recorre todas las cartas para imprimirlas
-                for (String ab : allCards) {
-                    System.out.println(ab);
-                }
-                
-                break;
-            case 6:
-                //Variables para poder imprimir y ser editadas luego
-                String monster = " ";
-                String trap = " "; 
-                String spell = " ";
-                
-                //ciclo for que busca el tipo de carta y las separa
-                for (int i = 0; i < cardName.size(); i++){
-                    String nameAll = cardName.get(i);
-                    String tipo = totalCards.get(nameAll);
-                    if (tipo.equals("Monstruo")){
-                        monster = monster + "\n" + nameAll + " (Monstruo)";
-                    }else if (tipo.equals("Trampa")){
-                        trap = trap + "\n" + nameAll + " (Trampa)";
-                    }else if (tipo.equals("Hechizo")){
-                        spell = spell + "\n" + nameAll + " (Hechizo)";
+                case 1:
+                    String name;
+                    System.out.println("Ingrese el nombre de la carta que desea agregar a su coleccion: ");
+                    name = read.next();
+                    if (totalCards.containsKey(name) == true){
+                        characters.add(name);
+                        String newCard = totalCards.get(name);
+                        totalCards.remove(name);
+                        cards.put(name, newCard);
+                        cardNamePlayer.add(name);
+                        System.out.println("La carta '" + name + "' se ha agregado a tu mazo.");
+                    }else{
+                        System.out.println("La carta que ingresaste no esta dentro del mazo general.");
                     }
-                }
-                System.out.println(monster + trap + spell);
+                    break;
+                
+                case 2:
+                    String name2;
+                    System.out.println("Ingrese el nombre de la carta para saber de que tipo es: ");
+                    name2 = read.next();
+                    System.out.println("La carta '" + name2 + "' es de tipo:" + totalCards.get(name2));
+                    break;
+
+                case 3:
+                    int cantMonster = 0;
+                    int cantTrap = 0;
+                    int cantSpells = 0;
+
+                    //Muestra las cartas puestas en el mazo 
+                    cards.forEach((nameC,type) -> System.out.println(nameC + " - " + type));
+
+                    //For que cuenta la cantidad de cada tipo de carta
+                    for (int item = 0; item < cards.size(); item++){
+                        if (cards.get(cardNamePlayer.get(item)).equals("Monstruo")){
+                            cantMonster +=1;      
+                        }else if(cards.get(totalCards.get(cardNamePlayer.get(item))).equals("Hechizo")){
+                            cantTrap +=1;
+                        }else if(cards.get(totalCards.get(cardNamePlayer.get(item))).equals("Trampa")){
+                            cantSpells +=1;
+                        }
+                    }
+                    //Resultado
+                    System.out.println("En tu mazo se encuentran: "
+                            + "\nMonstruos: " + cantMonster
+                            + "\nHechizos: " + cantTrap
+                            + "\nTrampas: " + cantSpells);
+                    break;
+                    
+                case 4:
+                    System.out.println("Huevos de acero");
+                    String monsterPlayer = " ";
+                    String trapPlayer = " "; 
+                    String spellPlayer = " ";
+
+                    //ciclo for que busca el tipo de carta y las separa
+                    for (int i = 0; i < cards.size(); i++){
+                        String nameAllPlayer = cardNamePlayer.get(i);
+                        String typePlayer = cards.get(nameAllPlayer);
+
+                    switch (typePlayer) {
+                        case "Monstruo":
+                            monsterPlayer = monsterPlayer + "\n" + nameAllPlayer + " (Monstruo)";
+                            break;
+                        case "Trampa":
+                            trapPlayer = trapPlayer + "\n" + nameAllPlayer + " (Trampa)";
+                            break;
+                        case "Hechizo":
+                            spellPlayer = spellPlayer + "\n" + nameAllPlayer + " (Hechizo)";
+                            break;
+                        }   
+                    }
+                    System.out.println(monsterPlayer + trapPlayer + spellPlayer);
+                    break;
+
+                case 5:
+                    //Ciclo for que recorre todas las cartas para imprimirlas
+                    for (String ab : allCards) {
+                        System.out.println(ab);
+                    }
+                    break;
+
+                case 6:
+                    //Variables para poder imprimir y ser editadas luego
+                    String monster = " ";
+                    String trap = " "; 
+                    String spell = " ";
+
+                    //ciclo for que busca el tipo de carta y las separa
+                    for (int i = 0; i < cardName.size(); i++){
+                        String nameAll = cardName.get(i);
+                        String type = totalCards.get(nameAll);
+
+                    switch (type) {
+                        case "Monstruo":
+                            monster = monster + "\n" + nameAll + " (Monstruo)";
+                            break;
+                        case "Trampa":
+                            trap = trap + "\n" + nameAll + " (Trampa)";
+                            break;
+                        case "Hechizo":
+                            spell = spell + "\n" + nameAll + " (Hechizo)";
+                            break;
+                        default:
+                            break;
+                        }
+                    }
+                    System.out.println(monster + trap + spell);
+            }
+            
         }
+        
+        
     }
     
 }
